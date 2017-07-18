18478119ffc4347d867d4f5c3ad0fbccfbaeac8c
crazyzlj <crazyzlj@gmail.com>
2017-07-18 12:17:16 +0800

    debug deploy

--- doc/ci/travis/linux_gcc48/install.sh
+++ doc/ci/travis/linux_gcc48/install.sh
@@ -2,16 +2,16 @@
 
 set -e
 ls
-mkdir utils_linux_gcc
+mkdir utils_linux_gcc48
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DINSTALL_PREFIX=../utils_linux_gcc
+cmake .. -DCMAKE_BUILD_TYPE=Release -DINSTALL_PREFIX=../utils_linux_gcc48
 make -j4
 sudo make install
 cd ..
 # zip the compiled binary
-zip -r utils_linux_gcc.zip utils_linux_gcc
+zip -r utils_linux_gcc48.zip utils_linux_gcc48
 # list all executable file
 ls
-cd utils_linux_gcc
+cd utils_linux_gcc48
 ls
--- doc/ci/travis/osx_xcode/install.sh
+++ doc/ci/travis/osx_xcode/install.sh
@@ -2,16 +2,16 @@
 
 set -e
 ls
-mkdir utils_mac_clang
+mkdir utils_osx_xcode
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DINSTALL_PREFIX=../utils_mac_clang
+cmake .. -DCMAKE_BUILD_TYPE=Release -DINSTALL_PREFIX=../utils_osx_xcode
 make -j4
 sudo make install
 cd ..
 # zip the compiled binary
-zip -r utils_mac_clang.zip utils_mac_clang
+zip -r utils_osx_xcode.zip utils_osx_xcode
 # list all executable file
 ls
-cd utils_mac_clang
+cd utils_osx_xcode
 ls
