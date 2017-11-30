1dabb925d852d257f486e9d07b03472c1278e87d
crazyzlj <crazyzlj@gmail.com>
2017-11-30 14:17:38 +0800

    add unittest based gtest

--- doc/ci/travis/linux_gcc48/script.sh
+++ doc/ci/travis/linux_gcc48/script.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 set -e
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DUNITTEST=1 -DRUNCOV=1 ..
+make
+make test
+cd ..
+ls
+codecov -t 002bf92f-6d1a-4e84-85ef-fa5c31ca4866 -X gcov
--- doc/ci/travis/osx_xcode/script.sh
+++ doc/ci/travis/osx_xcode/script.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 set -e
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DUNITTEST=1 ..
+make
+make test
+cd ..
+ls
