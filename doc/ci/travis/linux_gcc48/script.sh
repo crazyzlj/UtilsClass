bb1feb5a7ab4ae9701cadedd4059e93bb24a029d
crazyzlj <crazyzlj@gmail.com>
2017-11-30 18:22:48 +0800

    update

--- doc/ci/travis/linux_gcc48/script.sh
+++ doc/ci/travis/linux_gcc48/script.sh
@@ -5,7 +5,7 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DUNITTEST=1 -DRUNCOV=1 ..
 make
-make gcov_utilsclass
+make UnitTestCoverage
 cd ..
 ls
 codecov -t d9199af5-b185-48e9-b264-71bdc42b85f4 -X gcov
